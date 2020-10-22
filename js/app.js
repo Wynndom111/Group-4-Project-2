654abf6c77148d25e6319daad384416b6dd4f94c
vmdinolfo@gmail.com <vmdinolfo@gmail.com>
2020-10-21 19:50:59 -0500

    added query for geojson, save geojson file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-// Wynn Code Here
+// Creating map object
+var myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 11
+  });
 
-
-// Alex Code Here
-
-
-// Vince Code Here
-
-
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
